ad45e54d484ca2f1cbf84495e3b625bcac98dfc9
ThomasRebaudet <thomas.rebaudet@arece.eu>
2025-10-02 14:17:15 +0200

    re-organization of the repo with add_msg in root folder

---
+++ add_msg.sh
@@ -0,0 +1,8 @@
+export RTMAPS_SDKDIR="/opt/rtmaps/"
+cd /home/intempora/rtmaps-ros2-bridge/
+mkdir build
+cd build
+cmake .. && make
+sudo cp /home/intempora/rtmaps-ros2-bridge/build/rtmaps_ros2_custom_data_types.pck /opt/rtmaps/packages/
+sudo cp /home/intempora/rtmaps-ros2-bridge/build/rtmaps_ros2_bridge_core_function.pck /opt/rtmaps/packages/
+sudo cp /home/intempora/rtmaps-ros2-bridge/build/rtmaps_ros2_bridge.pck /opt/rtmaps/packages/
